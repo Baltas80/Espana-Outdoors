d496657531757c89aba56877e68efab8bc37ab34
Baltas80 <44073150+Baltas80@users.noreply.github.com>
2026-09-22 00:06:20 +0200

    feat: wire route planning screen into app

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -6,6 +6,7 @@ import '../features/home/home_page.dart';
 import '../features/map/map_page.dart';
 import '../features/offline/offline_page.dart';
 import '../features/profile/profile_page.dart';
+import '../features/routes/routes_page.dart';
 import '../features/safety/safety_page.dart';
 import 'theme.dart';
 
@@ -17,6 +18,7 @@ class EspanaOutdoorApp extends StatelessWidget {
     routes: [
       GoRoute(path: '/', builder: (_, __) => const HomePage()),
       GoRoute(path: '/explore', builder: (_, __) => const ExplorePage()),
+      GoRoute(path: '/routes', builder: (_, __) => const RoutesPage()),
       GoRoute(path: '/map', builder: (_, __) => const MapPage()),
       GoRoute(path: '/safety', builder: (_, __) => const SafetyPage()),
       GoRoute(path: '/offline', builder: (_, __) => const OfflinePage()),
