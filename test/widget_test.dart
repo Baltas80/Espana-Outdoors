4b352b97e6d48ce17f219bc93137a3869951f763
Baltas80 <44073150+Baltas80@users.noreply.github.com>
2026-09-22 15:03:54 +0200

    test: cover branded home experience

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,8 +10,10 @@ void main() {
     await tester.pumpAndSettle();
 
     expect(find.text('España Outdoor'), findsOneWidget);
+    expect(find.text('ESPAÑA OUTDOOR'), findsOneWidget);
     expect(find.text('Centro de seguridad'), findsOneWidget);
     expect(find.text('Antes de salir'), findsOneWidget);
     expect(find.text('Rutas'), findsOneWidget);
+    expect(find.bySemanticsLabel('España Outdoor'), findsOneWidget);
   });
 }
