6441f12bade5bdd86b18574447fc1b0f95ce74dc
Baltas80 <44073150+Baltas80@users.noreply.github.com>
2026-09-23 19:14:34 +0200

    feat(offline): verify downloaded packages before activation

--- lib/core/offline/offline_region_downloader.dart
+++ lib/core/offline/offline_region_downloader.dart
@@ -1,22 +1,18 @@
-import 'package:background_downloader/background_downloader.dart';
-import 'package:flutter/foundation.dart';
+import 'dart:io';
 
+import 'package:background_downloader/background_downloader.dart';
+
+import 'offline_package_verifier.dart';
 import 'offline_region.dart';
 
 class OfflineRegionDownloader {
   const OfflineRegionDownloader();
 
   Future<Transfer> start(OfflineRegion region) async {
-    if (kIsWeb) {
-      throw UnsupportedError(
-        'Background region downloads are not supported on Web; use the PWA download path.',
-      );
-    }
-
     final task = DownloadTask(
-      taskId: 'offline-region-${region.id}',
+      taskId: 'offline-region-' + region.id,
       url: region.downloadUrl.toString(),
-      filename: '${region.id}.pmtiles',
+      filename: region.id + '.pmtiles',
       directory: 'offline_regions',
       baseDirectory: BaseDirectory.applicationSupport,
       group: 'offline-regions',
@@ -26,11 +22,36 @@ class OfflineRegionDownloader {
       transferHints: const {TransferHint.largeFile},
       metaData: region.sha256 == null
           ? region.id
-          : '${region.id}|sha256=${region.sha256}',
+          : region.id + '|sha256=' + region.sha256!,
       displayName: region.name,
     );
-
     return FileDownloader().transfers.getOrStart(task);
+  }
+
+  Future<File> startAndVerify(
+    OfflineRegion region, {
+    bool requireChecksum = true,
+  }) async {
+    final transfer = await start(region);
+    final file = await transfer.file;
+    await const OfflinePackageVerifier().verifyFile(
+      file,
+      expectedSha256: region.sha256,
+      requireChecksum: requireChecksum,
+    );
+    return file;
+  }
+
+  Future<void> verifyExisting(
+    OfflineRegion region,
+    File file, {
+    bool requireChecksum = true,
+  }) {
+    return const OfflinePackageVerifier().verifyFile(
+      file,
+      expectedSha256: region.sha256,
+      requireChecksum: requireChecksum,
+    );
   }
 
   Future<bool> pause(Transfer transfer) => transfer.pause();
